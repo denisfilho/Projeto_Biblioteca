b5d9acbc3d01e4f152e830d70e45ec38b33eb1b1
denisfilho <denisferreira93@hotmail.com>
2024-01-19 23:06:11 -0300

    Implementando o Swagger: inserindo as rotas da entidade Student

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,21 @@ import express from "express";
 import { StudentRoutes } from "./modules/student/routes/student.routes";
 import { BookRoutes } from "./modules/book/routes/book.routes";
 import { ReservationRoutes } from "./modules/reservations/routes/reservation.route";
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "./swagger.json";
 
 export const app = express();
 app.use(express.json()); //receber JSON
+
+//app.get("/", (req, res) => {
+//  res.redirect("/api-docs");
+//});
+
 app.use("/students", StudentRoutes());
 app.use("/books", BookRoutes());
 app.use("/reservations", ReservationRoutes());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 export async function startWebServer() {
   return new Promise((resolve) => {
